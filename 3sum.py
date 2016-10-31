1932544be846db6caeb9b3439ee32a0f72fe7fee
sunyiyou <342065733@qq.com>
2016-10-31 17:01:47 +0800

    In 3sum.py:change list to set

--- 3sum.py
+++ 3sum.py
@@ -1,15 +1,25 @@
+import time
+
 class Solution(object):
     def threeSum(self, nums):
-        ret = []
+        ret = set([])
         l = len(nums)
+        print "%.6f" % time.time()
         for i in range(l):
-            dict = {}
+            st = set([])
             for j in range(i+1,l):
                 tmp = 0 - nums[i] - nums[j]
-                if nums[j] in dict:
-                    sl = sorted([nums[i],nums[j],tmp])
-                    if sl not in ret:
-                        ret.append(sl)
+                if nums[j] in st:
+                    sl = tuple(sorted([nums[i],nums[j],tmp]))
+                    ret.add(sl)
                 else:
-                    dict[tmp]=''
-        return ret
+                    st.add(tmp)
+        print "%.6f" % time.time()
+        return [list(tup) for tup in ret]
+
+
+
+if __name__ == '__main__':
+    result = Solution().threeSum([-1, 0, 1, 2, -1, -4])
+    print "%.6f" % time.time()
+    print result
